c454b01b8c82b9e0efd06ac403fc462fe0e37ca3
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-07-29 12:14:08 -0400

    added charaters property

--- Contents.swift
+++ Contents.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 // What's new in Swift 5.0
-// Swift Evolution (Resource) where users are able to update the swift language
+// Swift Evolution (Resource) where users are able to update the swift language and see changes in the swift language
+// Swift Evolution link: https://apple.github.io/swift-evolution/
 // Extended String Delimiters Link: https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html
 // Raw Strings
 // print hello with ""
@@ -16,3 +17,29 @@ print("new way in 5.0: \(message)", "Uses hastags")
 let ios = "iOS"
 let pmessage = #""Welcome to Pursuit's \#(ios) 6.0 cohort""#
 print(pmessage)
+
+
+/*
+ ==============================================================================
+ CHARACTERS
+ ==============================================================================
+ */
+
+// Link from apple docs https://developer.apple.com/documentation/swift/character
+
+let sentence = "🦢 Hello 1234 my n@Me i% ibrah✨?!"
+
+for char in sentence{
+    if char.isLetter {
+        print("letter: \(char)")
+    }
+    if char.isNumber {
+        print("Number: \(char)")
+    }
+    if char.isSymbol {
+        print("Symbol: \(char)")
+    }
+    if char.isPunctuation {
+        print("Punctuation: \(char)")
+    }
+}
